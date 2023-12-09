68cd4e4f5b6ec7912eff6a7d6886e221ed6df248
Epyonn <pdavisnguyen1@outlook.com>
2023-12-08 23:25:59 -0800

    Add State To Analytics

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Modal } from 'react-native';
+import { AppContext } from '../context/AppContext';
+
 
 const Analytics = () => {
+    const {state, dispatch} = useContext(AppContext);
+    const totalTime = state.totalTime;    
+    const [displayModal, setModal] = useState(false);
 
+    return (   
+    
+
+    )
 }
 
 export default Analytics;
